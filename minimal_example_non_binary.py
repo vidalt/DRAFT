quick fix on numerical and ordinal attribute example

minimal_example_non_binary.py
@@ -3,13 +3,14 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from utils import * 
 from datasets_infos import datasets_ohe_vectors, predictions, datasets_ordinal_attrs, datasets_numerical_attrs
+import numpy as np 
 
 script_seed = 42
 use_bootstrap = False
 sample_size = 1500
 train_size = 40
 dataset = "default_credit_numerical"
-debug_check = False # To perform additional checks and display information
+debug_check = True # To perform additional checks and display information
 
 # Load data information and dataset
 ohe_vector = datasets_ohe_vectors[dataset] # list of sublists indicating sets of binary attributes one-hot-encoding the same original one
@@ -71,11 +72,12 @@ x_sol = dict_res['reconstructed_data']
 
 # --------- (debug only / verify correctness of the reconstructed dataset information) --------------
 if debug_check:
-    checked_ohe = check_ohe(x_sol, ohe_vector)
+    x_sol_np = np.asarray(x_sol)
+    checked_ohe = check_ohe(x_sol_np, ohe_vector)
     print("(reconstruction) OHE verified: ", checked_ohe)
-    checked_ordinal_domains = check_domain(x_sol, ordinal_attrs)
+    checked_ordinal_domains = check_domain(x_sol_np, ordinal_attrs)
     print("(reconstruction) Ordinal attributes domains verified: ", checked_ordinal_domains)
-    checked_numerical_domains = check_domain(x_sol, numerical_attrs)
+    checked_numerical_domains = check_domain(x_sol_np, numerical_attrs)
     print("(reconstruction) Numerical attributes domains verified: ", checked_numerical_domains)
     if checked_ohe and checked_ordinal_domains and checked_numerical_domains:
         print("Reconstructed dataset information correct.\n")